3f233efc4427da550f2c29149df230451dca1f41
apudebnath <apu1578@gmail.com>
2022-01-27 21:11:23 +0600

    content updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const res = require('express/lib/response');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
